1655d77b170a237c48f0f32e698051768323593f
natuan9 <tuan3032001@gmail.com>
2024-12-14 16:59:24 +0700

    [MIG] web_datetime_picker_default_time: Migration to 18.0

--- web_datetime_picker_default_time/static/src/js/datepicker.esm.js
+++ web_datetime_picker_default_time/static/src/js/datepicker.esm.js
@@ -1,51 +1,122 @@
-/** @odoo-module **/
 /* Copyright 2024 Camptocamp
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl) */
-
-import {DateTimePicker} from "@web/core/datepicker/datepicker";
+import {DateTimePicker} from "@web/core/datetime/datetime_picker";
+import {DateTimePickerPopover} from "@web/core/datetime/datetime_picker_popover";
+import {MAX_VALID_DATE, MIN_VALID_DATE, clampDate, today} from "@web/core/l10n/dates";
+import {ensureArray} from "@web/core/utils/arrays";
 import {patch} from "@web/core/utils/patch";
-import {localization} from "@web/core/l10n/localization";
+const {DateTime} = luxon;
 
-patch(DateTimePicker.prototype, "DateTimePickerDefaultTime", {
-    onMounted() {
-        this._super.apply(this, arguments);
-        this.addPickerListener("change", ({date, oldDate}) => {
-            const default_time = this.props.defaultTime;
-            if (date && !oldDate && default_time) {
-                // FIXME: Consider TZ
-                date.set({
-                    hour: default_time.hour,
-                    minute: default_time.minute,
-                    second: default_time.second,
-                });
-                window.$(this.rootRef.el).datetimepicker("date", date);
-            }
-        });
-    },
-    isStrDate(input_string) {
-        return input_string.trim().length == localization.dateFormat.length;
-    },
-    customParseValue(input_value, options) {
-        const default_time = this.props.defaultTime;
-        let [res, error] = this.parseValueOriginal(input_value, options);
-        if (default_time && this.isStrDate(input_value)) {
-            const new_value = res.set({
-                hour: default_time.hour,
-                minute: default_time.minute,
-                second: default_time.second,
-            });
-            res = new_value;
+/**
+ * @param {Number} min
+ * @param {Number} max
+ */
+const numberRange = (min, max) => [...Array(max - min)].map((_, i) => i + min);
+
+/**
+ * @param {NullableDateTime | "today"} value
+ * @param {NullableDateTime | "today"} defaultValue
+ */
+const parseLimitDate = (value, defaultValue) =>
+    clampDate(
+        value === "today" ? today() : value || defaultValue,
+        MIN_VALID_DATE,
+        MAX_VALID_DATE
+    );
+
+// Time constants
+const HOURS = numberRange(0, 24).map((hour) => [hour, String(hour)]);
+const MINUTES = numberRange(0, 60).map((minute) => [
+    minute,
+    String(minute || 0).padStart(2, "0"),
+]);
+const SECONDS = [...MINUTES];
+
+patch(DateTimePicker.prototype, {
+    /**
+     * @param {DateTimePickerProps} props
+     */
+    onPropsUpdated(props) {
+        // Original logic handles
+        /** @type {[NullableDateTime] | NullableDateRange} */
+        this.values = ensureArray(props.value).map((value) =>
+            value && !value.isValid ? null : value
+        );
+        this.availableHours = HOURS;
+        this.availableMinutes = MINUTES.filter(
+            (minute) => !(minute[0] % props.rounding)
+        );
+        this.availableSeconds = props.rounding ? [] : SECONDS;
+        this.allowedPrecisionLevels = this.filterPrecisionLevels(
+            props.minPrecision,
+            props.maxPrecision
+        );
+
+        this.additionalMonth = props.range && !this.env.isSmall;
+        this.maxDate = parseLimitDate(props.maxDate, MAX_VALID_DATE);
+        this.minDate = parseLimitDate(props.minDate, MIN_VALID_DATE);
+        if (this.props.type === "date") {
+            this.maxDate = this.maxDate.endOf("day");
+            this.minDate = this.minDate.startOf("day");
         }
-        return [res, error];
+
+        if (this.maxDate < this.minDate) {
+            throw new Error(
+                `DateTimePicker error: given "maxDate" comes before "minDate".`
+            );
+        }
+
+        // OVERRIDE: Add defaultTime
+        const timeValues = this.values.map((val, index) =>
+            this.getCustomTimeValues(val, index)
+        );
+        if (props.range) {
+            this.state.timeValues = timeValues;
+        } else {
+            this.state.timeValues = [];
+            this.state.timeValues[props.focusedDateIndex] =
+                timeValues[props.focusedDateIndex];
+        }
+
+        this.shouldAdjustFocusDate = !props.range;
+        this.adjustFocus(this.values, props.focusedDateIndex);
+        this.handle12HourSystem();
+        this.state.timeValues = this.state.timeValues.map((timeValue) =>
+            timeValue.map(String)
+        );
     },
-    initFormat() {
-        this._super.apply(this, arguments);
-        this.parseValueOriginal = this.parseValue;
-        this.parseValue = this.customParseValue;
+
+    getCustomTimeValues(val, index) {
+        let timeValues = [];
+        if (index === 1) {
+            timeValues = [
+                val?.hour ||
+                    this.props.defaultEndTime?.hour ||
+                    DateTime.local().hour + 1,
+                (val || this.props.defaultEndTime)?.minute || 0,
+                (val || this.props.defaultEndTime)?.second || 0,
+            ];
+        } else {
+            timeValues = [
+                (
+                    val ||
+                    this.props.defaultTime ||
+                    this.props.defaultStartTime ||
+                    DateTime.local()
+                ).hour,
+                (val || this.props.defaultTime || this.props.defaultStartTime)
+                    ?.minute || 0,
+                (val || this.props.defaultTime || this.props.defaultStartTime)
+                    ?.second || 0,
+            ];
+        }
+
+        return timeValues;
     },
 });
 
-DateTimePicker.props = _.extend({}, DateTimePicker.props, {
+DateTimePicker.props = {
+    ...DateTimePicker.props,
     defaultTime: {
         type: Object,
         shape: {
@@ -55,4 +126,24 @@ DateTimePicker.props = _.extend({}, DateTimePicker.props, {
         },
         optional: true,
     },
-});
+    defaultStartTime: {
+        type: Object,
+        shape: {
+            hour: Number,
+            minute: Number,
+            second: Number,
+        },
+        optional: true,
+    },
+    defaultEndTime: {
+        type: Object,
+        shape: {
+            hour: Number,
+            minute: Number,
+            second: Number,
+        },
+        optional: true,
+    },
+};
+
+DateTimePickerPopover.props.pickerProps.shape = DateTimePicker.props;
--- web_datetime_picker_default_time/static/src/js/datetime_field.esm.js
+++ web_datetime_picker_default_time/static/src/js/datetime_field.esm.js
@@ -1,20 +1,137 @@
-/** @odoo-module **/
 /* Copyright 2024 Camptocamp
  * License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl) */
 
-import {DateTimeField} from "@web/views/fields/datetime/datetime_field";
+import {onWillRender, useState} from "@odoo/owl";
+import {useDateTimePicker} from "@web/core/datetime/datetime_hook";
+import {areDatesEqual} from "@web/core/l10n/dates";
 import {patch} from "@web/core/utils/patch";
+import {
+    DateTimeField,
+    dateRangeField,
+    dateTimeField,
+} from "@web/views/fields/datetime/datetime_field";
+import {
+    listDateRangeField,
+    listDateTimeField,
+} from "@web/views/fields/datetime/list_datetime_field";
 
-patch(DateTimeField.prototype, "DateTimeFieldDefaultTime", {
+patch(DateTimeField.prototype, {
+    setup() {
+        // Original logic handles
+        const getPickerProps = () => {
+            const value = this.getRecordValue();
+            /** @type {DateTimePickerProps} */
+            const pickerProps = {
+                value,
+                type: this.field.type,
+                range: this.isRange(value),
+            };
+            if (this.props.maxDate) {
+                pickerProps.maxDate = this.parseLimitDate(this.props.maxDate);
+            }
+            if (this.props.minDate) {
+                pickerProps.minDate = this.parseLimitDate(this.props.minDate);
+            }
+            if (!isNaN(this.props.rounding)) {
+                pickerProps.rounding = this.props.rounding;
+            } else if (!this.props.showSeconds) {
+                pickerProps.rounding = 0;
+            }
+            if (this.props.maxPrecision) {
+                pickerProps.maxPrecision = this.props.maxPrecision;
+            }
+            if (this.props.minPrecision) {
+                pickerProps.minPrecision = this.props.minPrecision;
+            }
+
+            // OVERRIDE: Add default time
+            if (this.props.defaultTime) {
+                pickerProps.defaultTime = this.defaultTime;
+            }
+            if (this.props.defaultStartTime) {
+                pickerProps.defaultStartTime = this.defaultStartTime;
+            }
+            if (this.props.defaultEndTime) {
+                pickerProps.defaultEndTime = this.defaultEndTime;
+            }
+            return pickerProps;
+        };
+
+        const dateTimePicker = useDateTimePicker({
+            target: "root",
+            showSeconds: this.props.showSeconds,
+            condensed: this.props.condensed,
+            get pickerProps() {
+                return getPickerProps();
+            },
+            onChange: () => {
+                this.state.range = this.isRange(this.state.value);
+            },
+            onApply: () => {
+                const toUpdate = {};
+                if (Array.isArray(this.state.value)) {
+                    [toUpdate[this.startDateField], toUpdate[this.endDateField]] =
+                        this.state.value;
+                } else {
+                    toUpdate[this.props.name] = this.state.value;
+                }
+
+                for (const fieldName in toUpdate) {
+                    if (
+                        areDatesEqual(
+                            toUpdate[fieldName],
+                            this.props.record.data[fieldName]
+                        )
+                    ) {
+                        delete toUpdate[fieldName];
+                    }
+                }
+
+                if (Object.keys(toUpdate).length) {
+                    this.props.record.update(toUpdate);
+                }
+            },
+        });
+        this.state = useState(dateTimePicker.state);
+        this.openPicker = dateTimePicker.open;
+
+        onWillRender(() => this.triggerIsDirty());
+    },
+
+    // Getter
     get defaultTime() {
         if (typeof this.props.defaultTime === "string") {
             return this.props.record.data[this.props.defaultTime];
         }
         return this.props.defaultTime;
     },
+
+    get defaultStartTime() {
+        if (typeof this.props.defaultStartTime === "string") {
+            return this.props.record.data[this.props.defaultStartTime];
+        }
+        return this.props.defaultStartTime;
+    },
+
+    get defaultEndTime() {
+        if (typeof this.props.defaultEndTime === "string") {
+            return this.props.record.data[this.props.defaultEndTime];
+        }
+        return this.props.defaultEndTime;
+    },
+
+    // OVERRIDE:remove automatic date calculation
+    async addDate(valueIndex) {
+        this.state.focusedDateIndex = valueIndex;
+        this.state.value = this.values;
+        this.state.range = true;
+
+        this.openPicker(valueIndex);
+    },
 });
 
-DateTimeField.props = _.extend({}, DateTimeField.props, {
+DateTimeField.props = {
+    ...DateTimeField.props,
     defaultTime: {
         type: [
             String,
@@ -30,13 +147,60 @@ DateTimeField.props = _.extend({}, DateTimeField.props, {
         ],
         optional: true,
     },
+    defaultStartTime: {
+        type: [
+            String,
+            {
+                type: Object,
+                shape: {
+                    hour: Number,
+                    minute: Number,
+                    second: Number,
+                },
+                optional: true,
+            },
+        ],
+        optional: true,
+    },
+    defaultEndTime: {
+        type: [
+            String,
+            {
+                type: Object,
+                shape: {
+                    hour: Number,
+                    minute: Number,
+                    second: Number,
+                },
+                optional: true,
+            },
+        ],
+        optional: true,
+    },
+};
+
+const super_dateTime_extractProps = dateTimeField.extractProps;
+dateTimeField.extractProps = ({attrs, options}, dynamicInfo) => ({
+    ...super_dateTime_extractProps({attrs, options}, dynamicInfo),
+    defaultTime: options.defaultTime,
 });
 
-const super_extractProps = DateTimeField.extractProps;
+const super_dateRange_extractProps = dateRangeField.extractProps;
+dateRangeField.extractProps = ({attrs, options}, dynamicInfo) => ({
+    ...super_dateRange_extractProps({attrs, options}, dynamicInfo),
+    defaultStartTime: options.defaultStartTime,
+    defaultEndTime: options.defaultEndTime,
+});
 
-DateTimeField.extractProps = ({attrs}) => {
-    return {
-        ...super_extractProps({attrs}),
-        defaultTime: attrs.options.defaultTime,
-    };
-};
+const super_listDateTime_extractProps = listDateTimeField.extractProps;
+listDateTimeField.extractProps = ({attrs, options}, dynamicInfo) => ({
+    ...super_listDateTime_extractProps({attrs, options}, dynamicInfo),
+    defaultTime: options.defaultTime,
+});
+
+const super_listDateRange_extractProps = listDateRangeField.extractProps;
+listDateRangeField.extractProps = ({attrs, options}, dynamicInfo) => ({
+    ...super_listDateRange_extractProps({attrs, options}, dynamicInfo),
+    defaultStartTime: options.defaultStartTime,
+    defaultEndTime: options.defaultEndTime,
+});
